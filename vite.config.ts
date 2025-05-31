a44f4aef677257b02be33b490347d8bd7c55a713
yermaka <ermak.080@mail.ru>
2025-05-31 19:06:09 +0800

    config path ~

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
+import {resolve} from 'path'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "~": "/src",
+     '~': resolve(__dirname, 'src'),
     },
   },
 })
